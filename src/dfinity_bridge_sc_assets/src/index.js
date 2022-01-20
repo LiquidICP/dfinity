7f9544e41fa795a82a7aa908bc89ddc2a22dd254
IntegralTeam <repo@sfxdx.ru>
2022-01-20 17:24:48 +0300

    removing unnecessary dependencies

--- src/dfinity_bridge_sc_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { dfinity_bridge_sc } from "../../declarations/dfinity_bridge_sc";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with dfinity_bridge_sc actor, calling the greet method
-  const greeting = await dfinity_bridge_sc.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
